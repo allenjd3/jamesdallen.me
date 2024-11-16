6651b46a248bcc0e417e416860ca068879b80400
James Allen <jacques2186@yahoo.com>
2024-11-16 07:05:24 -0500

    Add styles and blog

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primaryGreen: "#81B29A",
+				primaryYellow: "#F2CC8F",
+				primaryRedOrange: "#E07A5F",
+				background: "#F4F1DE",
+			},
+			fontFamily: {
+				sans: ['"Josefin Sans"', ...defaultTheme.fontFamily.sans],
+				display: ['"Bungee"', ...defaultTheme.fontFamily.sans],
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
